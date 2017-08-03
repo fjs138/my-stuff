09452620455d195dd12bf72d8d774b3ac9ea2a64
Frank <fjs138@gmail.com>
2017-08-03 15:25:35 -0400

    Finished creating jars

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "https://github.com/fjs138/my-stuff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot my-stuff.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+		[clj-http "2.0.0"]]
+  
+;;:main ^:skip-aot my-stuff.core
+  :main my-stuff.core 
+;;:target-path "target/%s"
+  
+;;:profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:dependencies [[ring/ring-devel "1.4.0"]]}}
+  :aot [my-stuff.core])
